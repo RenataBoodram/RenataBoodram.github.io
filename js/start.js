464f6282fbf4f60560ba8a4a61884fdd194a5128
RenataBoodram <RenataSBdm@gmail.com>
2015-09-06 20:51:56 -0400

    Submitted mobile icon changes and started using fa CDN.

---
+++ js/start.js
@@ -0,0 +1,26 @@
+// Check if the website is being viewed on a mobile device.
+var isMobile = {
+    Android: function() {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function() {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function() {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function() {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function() {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function() {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
+// If the user is viewing on a mobile device, change the icon size.
+if (isMobile.any()) {
+	$("span").switchClass("fa-2x", "fa-4x", 0);
+}
